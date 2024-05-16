5d286e0189d1ace4273f0c06ba7fa9176c062fe6
vitheka <vitorcoelhoft@gmail.com>
2024-05-16 19:30:37 -0300

    Add rabbitmq docker compose and create rabbitmq: Queue, RabbitAdmin and rabbitAdmin.initialize()

---
+++ src/main/java/com/pieropan/propostaapp/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,44 @@
+package com.pieropan.propostaapp.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
+        return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsNotificacao() {
+        return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsProposta() {
+        return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsNotificacao() {
+        return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
+        return  event -> rabbitAdmin.initialize();
+    }
+}
